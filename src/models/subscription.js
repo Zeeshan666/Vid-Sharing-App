ab886b42321147bc57cd6c078284c159e561f5e8
zeeshan666 <zeeshanmanzoor.zm@gmail.com>
2024-08-22 14:48:48 +0500

    minor fix on subscription model

--- src/models/subscription.js
+++ src/models/subscription.js
@@ -2,12 +2,14 @@ import mongoose, { Schema } from "mongoose";
 
 const SubscriptionSchema = new mongoose.Schema(
   {
-    subscriber: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  {
-    channel: Schema.Types.ObjectId,
-    ref: "User",
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
   {
     timestamps: true,
